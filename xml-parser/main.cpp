e128a436958d1e843815b5e2cbd532ed28d56106
Kraetzin <vshulyak2003@hotmail.com>
2014-07-16 22:15:18 +0100

    File parses into structure.

--- xml-parser/main.cpp
+++ xml-parser/main.cpp
@@ -3,14 +3,15 @@
 #include <vector>
 #include "rapidxml.hpp"
 #include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
 using namespace rapidxml;
 
 struct site
 {
-    char name[128];
-    char type[20];
+    string name;
+    string type;
     int x;
     int y;
 };
@@ -37,32 +38,40 @@ int main(void)
     doc.parse<0>(&xml_copy[0]);
 
     xml_node<> *pRoot = doc.first_node();
-    string coords;
     vector<string> strs;
     int id;
     for(xml_node<> *siteNode = pRoot->first_node("sites")->first_node(); siteNode; siteNode = siteNode->next_sibling())
     {
-        cout << siteNode->first_node("name")->value() << endl;
-        coords = siteNode->first_node("coords")->value();
+        try {
+            id = boost::lexical_cast<int>(siteNode->first_node("id")->value());
+        } catch( boost::bad_lexical_cast const& ) {
+            std::cout << "Error: input string was not valid" << std::endl;
+        }
+        site[id].name = siteNode->first_node("name")->value();
+        site[id].type = siteNode->first_node("type")->value();
+        string coords = siteNode->first_node("coords")->value();
         boost::split(strs, coords, boost::is_any_of(","));
-        cout << "x: " << strs[0] << " y: " << strs[1] << endl;
-
+        try {
+            site[id].x = boost::lexical_cast<int>(strs[0]);
+        } catch( boost::bad_lexical_cast const& ) {
+            std::cout << "Error: input string was not valid" << std::endl;
+        }
+        try {
+            site[id].y = boost::lexical_cast<int>(strs[1]);
+        } catch( boost::bad_lexical_cast const& ) {
+            std::cout << "Error: input string was not valid" << std::endl;
+        }
     }
+    cout << "Total number of sites " << id << endl;
 
-    /*
-    cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
-    cout << pRoot->first_node("sites")->first_node()->value() << endl;
-    pRoot = pRoot->first_node("sites");
-    cout << pRoot->first_node()->name() << endl;
-
-    xml_node<> *siteNode = pRoot->first_node();
-    cout << siteNode->name() << endl;
-
-    for(siteNode = pRoot->first_node(); siteNode; siteNode = siteNode->next_sibling())
+    for(int i=1;i<=id;i++)
     {
-        cout << siteNode->first_node("name")->value() << endl;
-        cout << siteNode->first_node("coords")->value() << endl;
+        cout << "Site ID: " << i << endl;
+        cout << "Name: " << site[i].name << endl;
+        cout << "Type: " << site[i].type << endl;
+        cout << "Coordinates:" << endl;
+        cout << "x: " << site[i].x << endl;
+        cout << "y: " << site[i].y << endl << endl;
     }
-    */
 
 }
