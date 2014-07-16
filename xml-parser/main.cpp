8e1df3955bb967e3129941932500f2916f4c8d2c
Kraetzin <vshulyak2003@hotmail.com>
2014-07-16 21:59:35 +0100

    Added splitting of strings using boost.

--- xml-parser/main.cpp
+++ xml-parser/main.cpp
@@ -2,15 +2,25 @@
 #include <fstream>
 #include <vector>
 #include "rapidxml.hpp"
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
 using namespace rapidxml;
+
+struct site
+{
+    char name[128];
+    char type[20];
+    int x;
+    int y;
+};
 
 //int main(int argc, char *argv[])
 int main(void)
 {
     string input_xml;
     string line;
+    struct site site[500];
 
     ifstream in("/home/astraeus/prog/learn/xml-parser/region1-legends.xml");
     //Read file into string variable
@@ -27,6 +37,19 @@ int main(void)
     doc.parse<0>(&xml_copy[0]);
 
     xml_node<> *pRoot = doc.first_node();
+    string coords;
+    vector<string> strs;
+    int id;
+    for(xml_node<> *siteNode = pRoot->first_node("sites")->first_node(); siteNode; siteNode = siteNode->next_sibling())
+    {
+        cout << siteNode->first_node("name")->value() << endl;
+        coords = siteNode->first_node("coords")->value();
+        boost::split(strs, coords, boost::is_any_of(","));
+        cout << "x: " << strs[0] << " y: " << strs[1] << endl;
+
+    }
+
+    /*
     cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
     cout << pRoot->first_node("sites")->first_node()->value() << endl;
     pRoot = pRoot->first_node("sites");
@@ -40,4 +63,6 @@ int main(void)
         cout << siteNode->first_node("name")->value() << endl;
         cout << siteNode->first_node("coords")->value() << endl;
     }
+    */
+
 }
